34864aba4b6ddc8a7727206fc89bbc20461ced27
Arison13 <arisonarias@gmail.com>
2022-11-22 17:02:57 -0500

    added port to server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const axios = require('axios');
-const port = process.env.PORT || 5000 ||  3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.listen(port, () => console.log(`Listening on port ${port}`));
